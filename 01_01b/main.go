3dbd5207790eb7bde651b7397e249204686f1db8
ali-abnair <109875898+ali-abnair@users.noreply.github.com>
2023-03-02 18:22:56 +0000

    challenge solved

--- 01_01b/main.go
+++ 01_01b/main.go
@@ -11,20 +11,17 @@ var expectedFormat = "2006-01-02"
 
 // parseTime validates and parses a given date string.
 func parseTime(target string) time.Time {
-
 	t, err := time.Parse(expectedFormat, target)
 	log.Println(t)
 	if err != nil || time.Now().After(t) {
 		log.Fatal(err)
 	}
-
 	return t
 }
 
 // calcSleeps returns the number of sleeps until the target.
 func calcSleeps(target time.Time) float64 {
 	t := time.Until(target).Hours() / 24
-
 	return t
 }
 
